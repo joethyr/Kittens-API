0b4b352d805b9ed6436cf546e09d41ae5f1e4d0c
joethyr <joethayr@gmail.com>
2022-06-03 19:14:05 -0600

    add all actions to Kitten controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,4 +1,32 @@
 class KittensController < ApplicationController
+
   def index
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_kitten
+  end
+
+  def kitten_params
+  end
+
 end
